b256143c589ca4b9bd8e41b00479b99f1f347e55
Markus Becker <mtib.becker@gmail.com>
2026-03-08 11:34:57 +0100

    Add dhl formula (placeholder; CI will update SHAs)

---
+++ Formula/dhl.rb
@@ -0,0 +1,25 @@
+class Dhl < Formula
+  desc "Git worktree workspace manager"
+  homepage "https://github.com/mtib/dhl"
+  version "latest"
+  license "MIT"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/mtib/dhl/releases/download/latest/dhl-aarch64-apple-darwin.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+    on_intel do
+      url "https://github.com/mtib/dhl/releases/download/latest/dhl-x86_64-apple-darwin.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+  end
+
+  def install
+    bin.install "dhl"
+  end
+
+  test do
+    assert_match "dhl", shell_output("#{bin}/dhl --help")
+  end
+end
